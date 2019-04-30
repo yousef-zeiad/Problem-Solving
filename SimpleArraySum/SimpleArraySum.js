174695491e5a09a562376183c868e0c35cef3929
Qais mohammad <zainzinc@Qaiss-iMac.local>
2019-04-30 12:26:37 +0300

    problemsolving-hackerrank

---
+++ SimpleArraySum/SimpleArraySum.js
@@ -0,0 +1,72 @@
+    
+// Function Description
+
+// Complete the simpleArraySum function in the editor below. It must return the sum of the array elements as an integer.
+
+// simpleArraySum has the following parameter(s):
+
+// ar: an array of integers
+// Input Format
+
+// The first line contains an integer,n , denoting the size of the array. 
+// The second line contains n space-separated integers representing the array's elements.
+
+
+
+
+
+
+
+
+
+
+
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(str => str.trim());
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the simpleArraySum function below.
+ */
+function simpleArraySum(ar) {
+   var total = 0;
+    for (var i = 0; i < ar.length; i++) {
+        total += ar[i];
+    }
+    return total;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const arCount = parseInt(readLine(), 10);
+
+    const ar = readLine().split(' ').map(arTemp => parseInt(arTemp, 10));
+
+    let result = simpleArraySum(ar);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
