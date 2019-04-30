b5add40c051111e3a0cb3c00ab6489f004540f65
Qais mohammad <zainzinc@Qaiss-iMac.local>
2019-04-30 12:50:04 +0300

    problemsolving hackerrank

---
+++ Diagonal Difference/Diagonal Difference.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the diagonalDifference function below.
+function diagonalDifference(arr) {
+    var sum1 = 0;
+    var sum2 = 0;
+    for (var i = 0; i < arr.length; i++) {
+        sum1 += arr[i][i];
+        sum2 += arr[arr.length - 1 - [i]] [i]
+        
+    }
+    return Math.abs(sum1 - sum2);
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    let arr = Array(n);
+
+    for (let i = 0; i < n; i++) {
+        arr[i] = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    }
+
+    const result = diagonalDifference(arr);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
---
+++ Plus Minus/Plus Minus.js
@@ -0,0 +1,51 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the plusMinus function below.
+function plusMinus(arr) {
+    var positive = 0;
+    var negative = 0;
+    var zero = 0;
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) {
+            positive++
+        } else if (arr[i] < 0) {
+            negative++
+        } else {
+            zero++;
+        }
+
+    }
+    console.log(positive /arr.length);
+    console.log(negative / arr.length);
+    console.log(zero / arr.length)
+}
+
+function main() {
+    const n = parseInt(readLine(), 10);
+
+    const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    plusMinus(arr);
+}
